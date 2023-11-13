2d8c7faee9e3b15ffa7a4cc3cb0e4469180dce2a
Aluno-AEDS <Aluno-AEDS@LAPTOP-18J36S36>
2023-11-13 12:25:57 +0000

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,19 @@
+namespace E9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(text: "Bem vindo ao c#");
+           
+
+
+                
+        }
+    }
+}
